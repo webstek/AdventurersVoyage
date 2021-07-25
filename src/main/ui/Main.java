5bf85cda98968675d07b55e1c69789b6442f9966
webstek <kyle.jr.webster@gmail.com>
2021-07-25 04:02:52 -0600

    Removed all previous work to make room for merge from "main" branch.

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -1,17 +1,6 @@
 package ui;
 
-import model.Player;
-import model.races.Human;
-
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        AdventurerVoyageApp advVoyApp = new AdventurerVoyageApp();
-        advVoyApp.characterCreation();
     }
-
-    //TODO Create character selection mode
-    //TODO The Story
-    //TODO Battle Class to deal with combat
 }
--- src/test/model/SizedListTest.java
+++
@@ -1,7 +0,0 @@
-package model;
-
-import model.SizedList;
-
-public class SizedListTest {
-    //TODO Write tests for SizedList methods
-}
